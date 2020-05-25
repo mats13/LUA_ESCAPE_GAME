b101905eac6506ebb7e0da882ece867bb7a58f45
Mats13 <lehats@laposte.net>
2020-05-26 00:02:25 +0200

    Avec les 4 animations et CSprite est un vrai objet agrégé à Hero

---
+++ src/class/CSprite.lua
@@ -0,0 +1,52 @@
+--[[ * PRINCIPES *
+  On importe tout le fichier de code, pas simplement une fonction factory
+
+--]]
+-- On crée une table qu'on renverra à la fin. En fait, une référence, mais on changera le contenu à chaque appel de new().
+local CSprite = {}
+
+-- Cette métatable fera le lien entre la table locale qu'on crée dans le new et la table exportable moduleAliens
+local csprite_mt = { __index = CSprite }
+
+-- Notre méthode Factory qui renvoie une fonction particulière de lua
+function CSprite.new()
+  
+  print("CSprite.new() / Création d'un instance de CSprite")
+  
+  local objSprite = {}
+  objSprite.x = 4
+  objSprite.image = nil
+  objSprite.framesAnimMatrix = {}
+  -- Le numéro d'animation courant
+  objSprite.idAnimCourant = 1
+  -- le numéro de frame courant (mais pas entier)
+  objSprite.idFrameAVirguleCourant = 1
+  
+  return setmetatable( objSprite, csprite_mt)
+end
+
+function CSprite:setImage(nomCompletFichier)
+  self.x = 5
+  self.image = love.graphics.newImage(nomCompletFichier)
+end
+
+function CSprite:addQuadAnim(idAnim, pX, pY, pWidth, pHeight)
+  -- Si l'idAnim est supérieur au nombre d'anims déjà dans le tableau, c'est qu'il faut le créer
+  if(idAnim > #self.framesAnimMatrix) then
+    -- On crée une nouvelle entrée 
+    self.framesAnimMatrix[idAnim] = {}
+  end
+  self.framesAnimMatrix[idAnim][#self.framesAnimMatrix[idAnim] + 1] = love.graphics.newQuad(pX, pY, pWidth, pHeight, self.image:getWidth(), self.image:getHeight())
+end
+ 
+function CSprite:affiche()
+  
+  print("Affichage")
+  print("X : "..self.x)
+  print("nb animations framesAnimMatrix : "..#self.framesAnimMatrix)
+  print("nb frame de animation 1 : "..#self.framesAnimMatrix[1])
+  -- print("image : "..self.image)
+end
+
+-- Lorsqu'à l'extérieur, on fait un "=require(module4)", ça récupère ce qui suit
+return CSprite 
--- src/hero.lua
+++ src/hero.lua
@@ -9,13 +9,12 @@ function Hero.setPosition(x, y)
   Hero.y = y
 end
 
-function Hero.load()
+function Hero.load(objSprite)
+  
+  Hero.objSprite = objSprite
   
   Hero.x = 0
   Hero.y = 0
-  Hero.image = nil
-  Hero.width = 0
-  Hero.height = 0
   -- On en déduit les dimensions du Hero
   -- Hero.width = Hero.image:getWidth()
   Hero.width = 20
@@ -24,42 +23,50 @@ function Hero.load()
   -- Hero.vx = 0
   -- Hero.vy = 0
    -- Une seule image contenant les quatres pxcel arts côte à côte
-  Hero.image = love.graphics.newImage("images/personnage-up-down.png")
-  -- Hero.image = love.graphics.newImage("images/hero.png")
+  -- Hero.image = love.graphics.newImage("images/personnage-up-down.png")
   
   -- print(Hero.image:getWidth())
   -- print(Hero.image:getHeight())
   -- On délimite 4 quads pour aller charger la bonne image
-  Hero.idFrameAVirgule = 1
+  -- Hero.idFrameAVirgule = 1
+  --[[
   Hero.frames = {}
   Hero.frames[1] = love.graphics.newQuad(0,0,20,20, Hero.image:getWidth(), Hero.image:getHeight())
   Hero.frames[2] = love.graphics.newQuad(20,0,20,20, Hero.image:getWidth(), Hero.image:getHeight())
   Hero.frames[3] = love.graphics.newQuad(40,0,20,20, Hero.image:getWidth(), Hero.image:getHeight())
   Hero.frames[4] = love.graphics.newQuad(60,0,20,20, Hero.image:getWidth(), Hero.image:getHeight())
-  
+  ]]
 end
 
 function Hero.update(dt)
-  -- * GESTION du N° de FRAME
-  Hero.idFrameAVirgule = Hero.idFrameAVirgule + 7*dt
-  if Hero.idFrameAVirgule >= #Hero.frames + 1 then
-    Hero.idFrameAVirgule = 1
+  -- * GESTION du N° de FRAME qu'on incrémente petit à petit
+  Hero.objSprite.idFrameAVirguleCourant = Hero.objSprite.idFrameAVirguleCourant + 7*dt
+  if Hero.objSprite.idFrameAVirguleCourant >= #Hero.objSprite.framesAnimMatrix + 1 then
+    Hero.objSprite.idFrameAVirguleCourant = 1
   end
   
   -- * GESTION des Fleches de contrôle
   if love.keyboard.isDown("up") then
+      -- L'id d'animation que l'on lance
+      Hero.objSprite.idAnimCourant = 2
       Hero.y = Hero.y - (50*dt)
   end
   
-  if love.keyboard.isDown("down") then
+  if love.keyboard.isDown("down") then    
+      -- L'id d'animation que l'on lance
+      Hero.objSprite.idAnimCourant = 1
       Hero.y = Hero.y + (50*dt)
   end
    
-  if love.keyboard.isDown("right") then
+  if love.keyboard.isDown("right") then  
+      -- L'id d'animation que l'on lance
+      Hero.objSprite.idAnimCourant = 4
       Hero.x = Hero.x + (50*dt)
   end  
   
-  if love.keyboard.isDown("left") then
+  if love.keyboard.isDown("left") then      
+      -- L'id d'animation que l'on lance
+      Hero.objSprite.idAnimCourant = 3
       Hero.x = Hero.x - (50*dt)
   end
 end
@@ -68,9 +75,9 @@ function Hero.draw()
   -- On aggrandi tout
   -- love.graphics.scale(4,4)
 
-  local idFrame = math.floor(Hero.idFrameAVirgule)
+  local idFrameEntier = math.floor(Hero.objSprite.idFrameAVirguleCourant)
   -- Affichage du Hero
-  love.graphics.draw(Hero.image, Hero.frames[idFrame], Hero.x, Hero.y, 0, 1, 1, Hero.width/2, Hero.height/2)
+  love.graphics.draw(Hero.objSprite.image, Hero.objSprite.framesAnimMatrix[Hero.objSprite.idAnimCourant][idFrameEntier], Hero.x, Hero.y, 0, 1, 1, Hero.width/2, Hero.height/2)
   -- love.graphics.draw(Hero.image, Hero.frames[idFrame], Hero.x, Hero.y)
   -- love.graphics.draw(Hero.image, Hero.x, Hero.y)
   
